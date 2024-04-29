178e3d99168446fefc7fb818b4ef04299f3b3ccd
Kreidli Ádám <akitlosz2000@gmail.com>
2024-04-29 22:40:29 +0200

    fix: as localStorage key value null

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ import { forEach } from 'lodash'
 
 /**
  * Acces localstorage and return the value of the key.
- * If you van to access nested key, you can use dot notation. `status.balance`
+ * If you van to access nested key, you can use dot notation. Example: `status.balance`
  * @param {string} key simple or nested key to find
  * @returns null or the value of the key
  */
@@ -10,21 +10,27 @@ export const readFromLocalStorage = (key) => {
   const keyMap = key.split('.')
   let current = JSON.parse(localStorage.getItem(keyMap[0]))
   forEach(keyMap.slice(1), (key, index) => {
-    if (index === keyMap.length - 2) {
-      current = current[key]
-    }
     if (!current || !current[key]) {
       return null
+    }
+    if (index === keyMap.length - 2) {
+      current = current[key]
     }
     current = current[key]
   })
   return current
 }
 
+/**
+ * Set localstorage and return the value of the key.
+ * You can set nested object as value. Example: `status.balance`
+ * @param {string} key simple or nested key to find
+ * @param {any} value to set as the last object value
+ */
 export const saveToLocalStorage = (key, value) => {
   const keyMap = key.split('.')
-  const parsedData = JSON.parse(localStorage.getItem(keyMap[0]))
-  let current = parsedData || {}
+  const parsedData = JSON.parse(localStorage.getItem(keyMap[0])) || {}
+  let current = parsedData
   forEach(keyMap.slice(1), (key, index) => {
     if (!current || !current[key]) {
       current[key] = {}
@@ -35,5 +41,4 @@ export const saveToLocalStorage = (key, value) => {
     current = current[key]
   })
   localStorage.setItem(keyMap[0], JSON.stringify(parsedData))
-  return current || null
 }
