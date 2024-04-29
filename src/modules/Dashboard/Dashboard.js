cee874c3631c3195deae7bb26d8b69eb6a8b95a7
Kreidli Ádám <akitlosz2000@gmail.com>
2024-04-29 21:39:11 +0200

    chore: main dashboard module

--- src/modules/Dashboard/Dashboard.js
+++ src/modules/Dashboard/Dashboard.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Card } from '../../components'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <div className='xl:flex justify-between m-6'>
+        <div className='xl:w-64 mb-6'>
+          <Card title="Total Balance">
+            <h1 className='text-3xl font-semibold'>HUF 0.00</h1>
+          </Card>
+        </div>
+        <div className='xl:w-64 mb-6'>
+          <Card title="Expense Activity">
+            <h1 className='text-3xl font-semibold'>HUF 0.00</h1>
+          </Card>
+        </div>
+        <div className='xl:w-64 mb-6'>
+          <Card title="Income Activity">
+            <h1 className='text-3xl font-semibold'>HUF 0.00</h1>
+          </Card>
+        </div>
+      </div>
+      <div id='daily-expense-line-chart' className='m-6'>
+        <Card title="Daily Expense">
+          <h1 className='text-3xl font-semibold'>ASD</h1>
+        </Card>
+      </div>
+      <div id='transaction-table' className='m-6'>
+        <Card title="Transaction">
+          <h1 className='text-3xl font-semibold'>ASD</h1>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
